81260fd638239f8acafee27a37f4120bb0a52108
Stephanie Velazquez <svelazquez@Stephanies-MacBook-Air.local>
2018-09-12 22:45:55 -0500

    render material on page

--- src/index.js
+++ src/index.js
@@ -4,17 +4,37 @@
 import sayHello from './hello';
 sayHello('World');
 
+const $ = require("jquery");
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+$(document).ready(function(){
+    console.log("hello");
+
+
+getMovies()
+    .then((movies) => {
+
+  $("h1").text('Here are all the movies:');
+  $(".movies").html("<table style='width:100%' class='movie-cont'>\n" +
+      "<tr>\n" +
+      "<th>ID</th>\n" +
+      "<th>Title</th>\n" +
+      "<th>Rating</th>\n" +
+      "</tr>");
+
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $(".movie-cont").append(`<tr><td>id#${id}</td> <td>${title}</td> <td>rating: ${rating}</td></tr>`);
   });
-}).catch((error) => {
+})
+    .catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+});
+
+
+
